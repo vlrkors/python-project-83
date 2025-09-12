fec5957fa09fe634d98dde025f830010302d687a
raev_va <vlrkors@gmail.com>
2025-09-12 09:56:07 +0300

    project-83 Stage4

--- database.sql
+++ database.sql
@@ -1,16 +1,18 @@
+-- URLs table: хранит сайты для проверки
 CREATE TABLE IF NOT EXISTS urls (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255) UNIQUE NOT NULL,
     created_at DATE DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Checks table: хранит проверки сайта
+-- На шаге 4 заполняются базовые поля: url_id (обязателен) и created_at (по умолчанию)
 CREATE TABLE IF NOT EXISTS url_checks (
     id SERIAL PRIMARY KEY,
-    url_id INT REFERENCES urls (id) NOT NULL,
+    url_id BIGINT REFERENCES urls (id) NOT NULL,
     status_code INT,
     h1 VARCHAR(255),
-    title VARCHAR(255) NOT NULL,
+    title VARCHAR(255),
     description VARCHAR(255),
     created_at DATE DEFAULT CURRENT_TIMESTAMP
 );
-
