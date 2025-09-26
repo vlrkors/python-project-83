a2970e4a0e83f21babd53018c3ad658c005bae7c
raev_va <vlrkors@gmail.com>
2025-09-26 09:04:31 +0300

    project-83 Stage5 Reformat test_parser.py

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -6,61 +6,73 @@ from page_analyzer.parser import get_data
 
 
 @pytest.mark.parametrize(
-    "html, expected",
-    [
-        pytest.param(
-            """
-            <html><head><title>Sample Title</title>
-            <meta name='description' content='Short description'></head>
-            <body><h1>Heading</h1></body></html>
-            """,
-            {"h1": "Heading", "title": "Sample Title", "description": "Short description"},
-            id="basic-elements",
-        ),
-        pytest.param(
-            """
-            <html><head><title>  Nested   Title  </title></head>
-            <body><h1><span>Nested</span><em>Heading</em></h1></body></html>
-            """,
-            {"h1": "NestedHeading", "title": "Nested   Title", "description": None},
-            id="nested-tags",
-        ),
-        pytest.param(
-            """
-            <html><head><meta name='description' content='   With surrounding spaces   '></head>
-            <body></body></html>
-            """,
-            {"h1": None, "title": None, "description": "With surrounding spaces"},
-            id="description-strip",
-        ),
-        pytest.param(
-            """
-            <html><head><title></title><meta name='description' content=''></head><body></body></html>
-            """,
-            {"h1": None, "title": "", "description": ""},
-            id="empty-values",
-        ),
-        pytest.param(
-            """
-            <html><head><title>{title}</title><meta name='description' content='{description}'></head>
-            <body><h1>{heading}</h1></body></html>
-            """.format(
-                title="T" * 300,
-                description="D" * 512,
-                heading="H" * 256,
-            ),
-            {
-                "h1": "H" * 256,
-                "title": "T" * 300,
-                "description": "D" * 512,
-            },
-            id="long-values",
-        ),
-    ],
+	"html, expected",
+	[
+		pytest.param(
+			"""
+			<html><head><title>Sample Title</title>
+			<meta name='description' content='Short description'></head>
+			<body><h1>Heading</h1></body></html>
+			""",
+			{
+				"h1": "Heading",
+				"title": "Sample Title",
+				"description": "Short description",
+			},
+			id="basic-elements",
+		),
+		pytest.param(
+			"""
+			<html><head><title>  Nested   Title  </title></head>
+			<body><h1><span>Nested</span><em>Heading</em></h1></body></html>
+			""",
+			{
+				"h1": "NestedHeading",
+				"title": "Nested   Title",
+				"description": None,
+			},
+			id="nested-tags",
+		),
+		pytest.param(
+			"""
+			<html><head><meta name='description' content='   With surrounding spaces   '></head>
+			<body></body></html>
+			""",
+			{
+				"h1": None,
+				"title": None,
+				"description": "With surrounding spaces",
+			},
+			id="description-strip",
+		),
+		pytest.param(
+			"""
+			<html><head><title></title><meta name='description' content=''></head><body></body></html>
+			""",
+			{"h1": None, "title": "", "description": ""},
+			id="empty-values",
+		),
+		pytest.param(
+			"""
+			<html><head><title>{title}</title><meta name='description' content='{description}'></head>
+			<body><h1>{heading}</h1></body></html>
+			""".format(
+				title="T" * 300,
+				description="D" * 512,
+				heading="H" * 256,
+			),
+			{
+				"h1": "H" * 256,
+				"title": "T" * 300,
+				"description": "D" * 512,
+			},
+			id="long-values",
+		),
+	],
 )
 def test_get_data_returns_expected(html, expected):
-    response = types.SimpleNamespace(text=html)
+	response = types.SimpleNamespace(text=html)
 
-    result = get_data(response)
+	result = get_data(response)
 
-    assert result == expected
+	assert result == expected
